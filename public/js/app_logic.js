297e9ac8744c16a5a5e1601acbf6486b2eacbb5f
Christoph Woergoetter <christoph.woergoetter@gmail.com>
2019-01-17 21:26:11 +0100

    better looking (modern) layout

--- public/js/app_logic.js
+++ public/js/app_logic.js
@@ -10,144 +10,181 @@ var Bgpio = Bgpio || {};
 
 Bgpio.workspace = null;
 Bgpio.DEBUG = true;
-Bgpio.PIN_COUNT = 26;
+Bgpio.PIN_COUNT = 40;
 
 Bgpio.init = function () {
-  Bgpio.workspace = Blockly.inject('blocklyDiv', {
-      media: 'blockly/media/',
-      toolbox: document.getElementById('toolbox')
-  });
-  Blockly.Xml.domToWorkspace(Bgpio.workspace,
-      document.getElementById('startBlocks'));
-  Bgpio.workspace.addChangeListener(Bgpio.renderPythonCode);
+    Bgpio.workspace = Blockly.inject('blocklyDiv', {
+        media: 'blockly/media/'
+        , toolbox: document.getElementById('toolbox')
+        , comments: true
+        , horizontalLayout : true 
+        , toolboxPosition : 'start'
+        , css : true
+        , grid: {
+            spacing: 20
+            , length: 3
+            , colour: '#ccc'
+            , snap: true
+        }
+        , trashcan: true
+        , zoom: {
+            controls: true
+            , wheel: true
+            , startScale: 1.0
+            , maxScale: 3
+            , minScale: 0.3
+            , scaleSpeed: 1.2
+        }
+    });
+    Blockly.Xml.domToWorkspace(Bgpio.workspace
+        , document.getElementById('startBlocks'));
+    Bgpio.workspace.addChangeListener(Bgpio.renderPythonCode);
 
-  Bgpio.clearJsConsole();
-  Bgpio.WebSocket.init();
+    Bgpio.clearJsConsole();
+    Bgpio.WebSocket.init();
 };
 
 window.addEventListener('load', function load(event) {
-  window.removeEventListener('load', load, false);
-  Bgpio.init();
+    window.removeEventListener('load', load, false);
+    Bgpio.init();
 });
 
 Bgpio.runMode = {
-  selected: 0,
-  types: ['Simulation', 'Execution'],
-  getSelectedMode: function() { return this.types[this.selected]; },
-  selectMode: function(id) { this.selected = id;  this.updateState_(); },
-  selectNextMode:  function() {
-        this.selected++;
+    selected: 0
+    , types: ['Simulation', 'Execution']
+    , connectionAdr: ''
+    , getSelectedMode: function () {
+        return this.types[this.selected];
+    }
+    , selectMode: function (id) {
+        this.selected = id;
+        this.updateState_();
+    }
+    , selectNextMode: function (mode) {
+        this.selected = mode;
         if (this.selected >= this.types.length) this.selected = 0;
         this.updateState_();
-      },
-  debugInit: Bgpio.JsInterpreter.debugInit,
-  debugStep: Bgpio.JsInterpreter.debugStep,
-  run: Bgpio.JsInterpreter.run,
-  stop: Bgpio.JsInterpreter.stop,
-  updateState_: function() {
-        for (var i = 0; i < this.types.length; i++) {
-          var modeText = document.getElementById('mode' + this.types[i]);
-          if (i === this.selected) {
-            modeText.style.display = 'inline';
-          } else {
-            modeText.style.display = 'none';
-          }
+    }
+    , debugInit: Bgpio.JsInterpreter.debugInit
+    , debugStep: Bgpio.JsInterpreter.debugStep
+    , run: Bgpio.JsInterpreter.run
+    , stop: Bgpio.JsInterpreter.stop
+    , updateState_: function () {
+        /*for (var i = 0; i < this.types.length; i++) {
+            var modeText = document.getElementById('mode' + this.types[i]);
+            if (i === this.selected) {
+                modeText.style.display = 'inline';
+            } else {
+                modeText.style.display = 'none';
+            }
+        }*/
+        var btn = document.getElementById('modeBtn');
+        var btn_icon = btn.firstChild;
+        if (Bgpio.runMode.selected == 0) {
+          btn_icon.classList.remove("fab");
+          btn_icon.classList.remove("fa-raspberry-pi");
+          btn_icon.classList.add("fas");
+          btn_icon.classList.add("fa-bug");
+        } else {
+          btn_icon.classList.remove("fas");
+          btn_icon.classList.remove("fa-bug");
+          btn_icon.classList.add("fab");
+          btn_icon.classList.add("fa-raspberry-pi");
         }
-        var simulationContent = document.getElementById('simulationContentDiv');
+              var simulationContent = document.getElementById('simulationContentDiv');
         var executionContent = document.getElementById('executionContentDiv');
         if (this.selected === 0) {
-          simulationContent.style.display = 'block';
-          executionContent.style.display = 'none';
-          this.debugInit = Bgpio.JsInterpreter.debugInit;
-          this.debugStep = Bgpio.JsInterpreter.debugStep;
-          this.run = Bgpio.JsInterpreter.run;
-          this.stop = Bgpio.JsInterpreter.stop;
-          document.getElementById('debugInitButton').disabled = false;
+            simulationContent.style.display = 'block';
+            executionContent.style.display = 'none';
+            this.debugInit = Bgpio.JsInterpreter.debugInit;
+            this.debugStep = Bgpio.JsInterpreter.debugStep;
+            this.run = Bgpio.JsInterpreter.run;
+            this.stop = Bgpio.JsInterpreter.stop;
+            document.getElementById('debugInitButton').disabled = false;
         } else {
-          simulationContent.style.display = 'none';
-          executionContent.style.display = 'block';
-          this.debugInit = Bgpio.PythonInterpreter.debugInit;
-          this.debugStep = Bgpio.PythonInterpreter.debugStep;
-          this.run = Bgpio.PythonInterpreter.run;
-          this.stop = Bgpio.PythonInterpreter.stop;
-          document.getElementById('debugInitButton').disabled = true;
+            simulationContent.style.display = 'none';
+            executionContent.style.display = 'block';
+            this.debugInit = Bgpio.PythonInterpreter.debugInit;
+            this.debugStep = Bgpio.PythonInterpreter.debugStep;
+            this.run = Bgpio.PythonInterpreter.run;
+            this.stop = Bgpio.PythonInterpreter.stop;
+            document.getElementById('debugInitButton').disabled = true;
         }
-      },
-};
+    }
+, };
 
 /*******************************************************************************
  * Blockly related
  ******************************************************************************/
- Bgpio.generateJavaScriptCode = function() {
-  return Blockly.JavaScript.workspaceToCode(Bgpio.workspace);
+Bgpio.generateJavaScriptCode = function () {
+    return Blockly.JavaScript.workspaceToCode(Bgpio.workspace);
 };
 
-Bgpio.generatePythonCode = function() {
-  return Blockly.Python.workspaceToCode(Bgpio.workspace);
+Bgpio.generatePythonCode = function () {
+    return Blockly.Python.workspaceToCode(Bgpio.workspace);
 };
 
-Bgpio.generateXml = function() {
-  var xmlDom = Blockly.Xml.workspaceToDom(Bgpio.workspace);
-  var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
-  return xmlText;
+Bgpio.generateXml = function () {
+    var xmlDom = Blockly.Xml.workspaceToDom(Bgpio.workspace);
+    var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+    return xmlText;
 };
 
-Bgpio.renderPythonCode = function() {
-  // Only regenerate the code if a block is not being dragged
-  if (Blockly.dragMode_ != 0) {
-    return;
-  }
-  // Render Python Code with latest change highlight and syntax highlighting
-  var pyPre = document.getElementById('pythonCodePre');
-  pyPre.textContent = Bgpio.generatePythonCode();
-  pyPre.innerHTML = prettyPrintOne(pyPre.innerHTML, 'py', false);
+Bgpio.renderPythonCode = function () {
+    // Only regenerate the code if a block is not being dragged
+    if (Blockly.dragMode_ != 0) {
+        return;
+    }
+    // Render Python Code with latest change highlight and syntax highlighting
+    var pyPre = document.getElementById('pythonCodePre');
+    pyPre.textContent = Bgpio.generatePythonCode();
+    pyPre.innerHTML = prettyPrintOne(pyPre.innerHTML, 'py', false);
 };
 
 /*******************************************************************************
  *  Right content related
  ******************************************************************************/
-Bgpio.setPinDefaults = function() {
- for (var i = 1; i <= Bgpio.PIN_COUNT; i++) {
-   document.getElementById('pin' + i).className = 'pinDefault';
- }
+Bgpio.setPinDefaults = function () {
+    for (var i = 1; i <= Bgpio.PIN_COUNT; i++) {
+        document.getElementById('pin' + i).className = 'pinDefault';
+    }
 };
 
-Bgpio.setPinDigital = function(pinNumber, isPinHigh) {
-  var pin = document.getElementById('pin' + pinNumber);
-  pin.className = isPinHigh ? 'pinDigitalHigh' : 'pinDigitalLow';
+Bgpio.setPinDigital = function (pinNumber, isPinHigh) {
+    var pin = document.getElementById('pin' + pinNumber);
+    pin.className = isPinHigh ? 'pinDigitalHigh' : 'pinDigitalLow';
 };
 
-Bgpio.appendTextJsConsole = function(text) {
-  var consoleId = (Bgpio.runMode.getSelectedMode() == Bgpio.runMode.types[1]) ?
-    'pythonConsolePre' : 'jsConsolePre';
-  if (Bgpio.DEBUG) console.log('Print in console with id: ' + consoleId);
-  var jsConsole = document.getElementById(consoleId);
-  jsConsole.textContent += text + '\n';
+Bgpio.appendTextJsConsole = function (text) {
+    var consoleId = (Bgpio.runMode.getSelectedMode() == Bgpio.runMode.types[1]) ?
+        'pythonConsolePre' : 'jsConsolePre';
+    if (Bgpio.DEBUG) console.log('Print in console with id: ' + consoleId);
+    var jsConsole = document.getElementById(consoleId);
+    jsConsole.textContent += text + '\n';
 };
 
-Bgpio.clearJsConsole = function(text) {
-  var consoleId = (Bgpio.runMode.getSelectedMode() == Bgpio.runMode.types[1]) ?
-    'pythonConsolePre' : 'jsConsolePre';
-  if (Bgpio.DEBUG) console.log('Clear console with id: ' + consoleId);
-  var jsConsole = document.getElementById(consoleId);
-  if (Bgpio.runMode.getSelectedMode() == Bgpio.runMode.types[0])
-    jsConsole.textContent = 'Simulated print output.\n';
-  else
-    jsConsole.textContent = '';
+Bgpio.clearJsConsole = function (text) {
+    var consoleId = (Bgpio.runMode.getSelectedMode() == Bgpio.runMode.types[1]) ?
+        'pythonConsolePre' : 'jsConsolePre';
+    if (Bgpio.DEBUG) console.log('Clear console with id: ' + consoleId);
+    var jsConsole = document.getElementById(consoleId);
+    if (Bgpio.runMode.getSelectedMode() == Bgpio.runMode.types[0])
+        jsConsole.textContent = 'Simulated print output.\n';
+    else
+        jsConsole.textContent = '';
 };
 
 /*******************************************************************************
  * Other
  ******************************************************************************/
-Bgpio.getRaspPiIp = function() {
-  var ipField = document.getElementById('raspPiIp');
-  var ip = ipField.value;
-  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip) 
-      || ip.toLowerCase() == 'localhost'
-      || ip.toLowerCase() == 'raspberrypi.local') {
-    ipField.style.color = "green";
-    return ipField.value;
-  }
-  ipField.style.color = "red";
-  return null;
+Bgpio.getRaspPiIp = function () {
+    //var ipField = document.getElementById('raspPiIp');
+    var ipField = Bgpio.runMode.connectionAdr;
+    var ip = ipField.value;
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip) || ip.toLowerCase() == 'localhost' || ip.toLowerCase() == 'raspberrypi.local') {
+        ipField.style.color = "green";
+        return ipField.value;
+    }
+    ipField.style.color = "red";
+    return null;
 };
