2c50d201aeb83a893886a5b85bd8ba7f2d4b18c3
Christoph Woergoetter <christoph.woergoetter@gmail.com>
2019-04-25 20:37:56 +0200

    added dht11 block

--- public/blocks/gpio.js
+++ public/blocks/gpio.js
@@ -341,6 +341,58 @@ Blockly.Python['dht22'] = function(block) {
 
 
 
+Blockly.Blocks['dht11'] = {
+  /**
+   * Description.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Measure with DHT11 on pin#")
+        .appendField(new Blockly.FieldDropdown(PINS), 'PIN');
+    this.setOutput(true, "Array");
+    this.setColour(GPIO_HUE);
+    this.setInputsInline(false);
+    this.setPreviousStatement(false, null);
+    this.setNextStatement(false, null);
+    this.setTooltip("DHT11 temperature-humidity sensor");
+    this.setHelpUrl("https://www.adafruit.com/product/385");
+  }
+};
+
+/**
+ * Description.
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+Blockly.JavaScript['dht11'] = function(block) {
+  var code = '[21.7, 63.42]\n'; // return some dummy result values
+  return [code, Blockly.JavaScript.ORDER_MEMBER];
+};
+
+/**
+ * Description.
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+Blockly.Python['dht11'] = function(block) {
+  var pin = block.getFieldValue('PIN');
+  // Very hackish way to get the BMC pin number, need to create a proper look
+  // up dicionary with a function to generate the dropdown
+  for (var i = 0; i < PINS.length; i++) {
+    if (PINS[i][1] == pin) {
+      pin = PINS[i][0];
+      break;
+    }
+  }
+  Blockly.Python.definitions_['import_dht11'] = 'from dhtxx import dht11';
+  Blockly.Python.definitions_['declare_dht11_pin' + pin] =
+      'dht11_pin' + pin + ' = DHT11(' + pin + ')';
+  var code = 'dht11_pin' + pin + '.get_result_once()';
+  return [code, Blockly.JavaScript.ORDER_MEMBER];
+};
+
+
 Blockly.Blocks['hc_sr04'] = {
   /**
    * Description.
